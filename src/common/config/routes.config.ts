83bbe0bf8df646c4dbec50dad21b38e0ed54e8e2
fayyozbobokulov <b.fayyoz2000@gmail.com>
2023-04-26 04:03:03 +0500

    cnofiguration is corrected

--- src/common/config/routes.config.ts
+++ src/common/config/routes.config.ts
@@ -3,6 +3,7 @@ import { CommonRoutesConfig } from '../common.routes.config';
 import { UsersRoutes } from '../../users/users.routes.config';
 import { AuthRoutes } from '../../auth/auth.routes.config';
 import { PaymentRoutes } from '../../payments/payments.routes.config';
+import { NotificationRoutes } from '../../notificaitons/notifications.routes.config';
 
 export function registerRoutes(
 	routes: Array<CommonRoutesConfig>,
@@ -11,4 +12,5 @@ export function registerRoutes(
 	routes.push(new AuthRoutes(app));
 	routes.push(new UsersRoutes(app));
 	routes.push(new PaymentRoutes(app));
+	routes.push(new NotificationRoutes(app));
 }
--- src/notificaitons/controllers/notifications.controller.ts
+++ src/notificaitons/controllers/notifications.controller.ts
@@ -69,3 +69,5 @@ class NotificaionsController {
 		}
 	}
 }
+
+export default new NotificaionsController();
